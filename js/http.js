575de4646c7d9e82927e6a363c17dfce909221ce
niggtim <niggtim@students.zhaw.ch>
2022-11-27 23:33:49 +0100

    Add simple css for animation-container and its elements.

--- js/http.js
+++ js/http.js
@@ -1,4 +1,4 @@
-const URL = 'localhost:12341'
+const URL = 'http://localhost:12341'
 const BASE = '/animations'
 
 export const All_ANIMATIONS = BASE
@@ -14,8 +14,56 @@ export const TYPE = {
 }
 
 export const request = (type, url, next) => {
-    fetch(url)
-    .then(data => {next(data)})
-    .then(res => {console.log(res)})
+    throw 'METHOD NOT YET IMPLEMENTED'
 }
 
+export const getAllAnimations = (url, succ, err) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            succ(JSON.parse(xhr.responseText))
+        } else {
+            err(xhr.response)
+        }
+    };
+
+    xhr.send();
+}
+
+export const getAllAnimationsTest = (url, succ, err) => {
+    console.log('well here we are')
+    let json = `[
+        {
+            "Key": "corona_spread_active_cases",
+            "Name": "Corona Spread Active Cases",
+            "Description": "This animation displays all active cases of Covid-19 infections for any given day in every tracked country starting on January 22nd, 2020. DISCLAIMER: The active infection count for each US state is estimated! We do not have the exact active infection count per state!",
+            "AnimationType": "custom",
+            "CommandData": null
+        },
+        {
+            "Key": "website_windy",
+            "Name": "Windy",
+            "Description": "Displays live weather.",
+            "AnimationType": "website",
+            "CommandData": null
+        },
+        {
+            "Key": "video_firstvideo",
+            "Name": "First Video",
+            "Description": "First video to display.",
+            "AnimationType": "video",
+            "CommandData": null
+        },
+        {
+            "Key": "co2_co2animation",
+            "Name": "Co2 Animation",
+            "Description": "Displays the world at a certain Co2-level.",
+            "AnimationType": "co2",
+            "CommandData": null
+        }
+    ]`
+
+    succ(JSON.parse(json))
+}
--- js/main.js
+++ js/main.js
@@ -2,12 +2,27 @@ import * as http from './http.js'
 
 function loadAnimations() {
     console.log('loading all animations')
+    http.getAllAnimationsTest(http.All_ANIMATIONS, createAnimationElements, error)
+}
 
-    let animations = document.getElementById('animations')
+function createAnimationElements(json) {
+    let animationsContainer = document.getElementById('animations-container')
 
-    // call backend and load all animaitons
-    
-    // create element for each animation
+    for (let index in json) {
+        let animation = json[index]
+
+        let child = document.createElement('div')
+        child.classList.add('animation')
+        
+        let text = document.createTextNode(animation['Name'])
+        child.appendChild(text)
+
+        animationsContainer.appendChild(child)
+    }
+}
+
+function error(res) {
+    console.log('an error occured with response: ' + res)
 }
 
 loadAnimations()
