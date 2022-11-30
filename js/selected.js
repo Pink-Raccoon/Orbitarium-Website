57dd945b8f182fd74510cc790a190f9aba376301
niggtim <niggtim@students.zhaw.ch>
2022-11-30 01:22:51 +0100

    Clear selection after reselecting an element.

--- js/selected.js
+++ js/selected.js
@@ -4,24 +4,33 @@ const selectAnimationDiv = document.getElementById('animation-selected')
 
 function makeSelectFunction(animation) {
     function select() {
-        console.log('selected: ' + animation['Key'])
+        clearSelection()
         selectAnimationDiv.classList.remove('hidden')
 
         let div = document.createElement('div')
         div.id = animation['Key']
         
         let title = document.createTextNode(animation['Name'])
-        let text = document.createTextNode('Description' + animation['Description'])
+        let description = document.createTextNode('Description' + animation['Description'])
 
         div.appendChild(title)
-        div.appendChild(text)
+        div.appendChild(description)
+
+        currentSelection = div
         selectAnimationDiv.appendChild(div)
     }
 
     return select
 }
 
-
+function clearSelection() {
+    if (currentSelection === null) {
+        return
+    }
+    
+    selectAnimationDiv.removeChild(currentSelection)
+    currentSelection = null
+}
 
 export {
     makeSelectFunction as makeSelectFunction
