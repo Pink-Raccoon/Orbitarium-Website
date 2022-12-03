4a505203f563f75f80446f1024d4386bfffdda8b
niggtim <niggtim@students.zhaw.ch>
2022-12-03 15:24:03 +0100

    Add information and events to co2 slider.

--- js/animations/co2.animation.js
+++ js/animations/co2.animation.js
@@ -1,12 +1,38 @@
+import * as http from "./../http.js"
+
 const fillCo2Template = (template, data) => {
     template.querySelector('.title').textContent = data['Name']
-
     template.querySelector('#description').append(document.createTextNode(data['Description']))
+    
+    let slider =template.querySelector('#adaptations .slider input')
+    slider.min = data['Adaption']['Slider']['MinValue']
+    slider.max = data['Adaption']['Slider']['MaxValue']
+    slider.value = data['Adaption']['Slider']['Value']
+
+    slider.addEventListener('change', (event) => {
+        sendSliderChangeValue(event.srcElement.value)
+    })
+
+    let sliderValue = template.querySelector('#slider-value')
+    slider.addEventListener('input', (event) => {
+        sliderValue.innerHTML = event.srcElement.value 
+    })
 
     let infoNodes = template.querySelectorAll('.info')
     infoNodes[0].querySelector('.info-content').textContent = data['Information']['SeaLevel']
     infoNodes[1].querySelector('.info-content').textContent = data['Information']['Temp']
     infoNodes[2].querySelector('.info-content').textContent = data['Information']['Year']
+}
+
+const sendSliderChangeValue = (value) => {
+    http.POST(http.ADAPT_ANIMATION, 
+        {
+            CommandName: "update_co2_level",
+            Data: {
+                ppm: value
+            }
+        },
+        () => console.log('successfully updated'))
 }
 
 
